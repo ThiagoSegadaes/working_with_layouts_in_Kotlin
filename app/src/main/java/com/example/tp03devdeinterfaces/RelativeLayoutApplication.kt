b6ddf6f1288480737ce89d0af590efcf7c229d92
ThiagoSegadaes <Thiagopace2002@gmail.com>
2022-06-06 20:28:50 -0300

    first commit

---
+++ app/src/main/java/com/example/tp03devdeinterfaces/RelativeLayoutApplication.kt
@@ -0,0 +1,11 @@
+package com.example.tp03devdeinterfaces
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RelativeLayoutApplication : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_relative_layout_application)
+    }
+}
